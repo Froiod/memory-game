92bda500d4b85edf9d34d60d95714f4eb0a4c424
Paolo Guray <froiodpaolo@gmail.com>
2023-01-11 21:07:18 +0800

    added card images and shuffled

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import './App.css';
 
+const cardImages = [
+  { 'src': '/img/helmet.png' },
+  { 'src': '/img/potion.png' },
+  { 'src': '/img/ring.png' },
+  { 'src': '/img/scroll.png' },
+  { 'src': '/img/shield.png' },
+  { 'src': '/img/sword.png' },
+]
+
 function App() {
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({...card, id: Math.random()}) )
+  }
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
