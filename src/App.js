d79c9d86e2a64b55e8b19726034a7cbe5edc9021
Paolo Guray <froiodpaolo@gmail.com>
2023-01-11 22:34:26 +0800

    added click event on cover img

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ function App() {
 
   const [cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
 
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
@@ -25,13 +27,21 @@ function App() {
     setTurns(0)
   }
 
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
       <button onClick={shuffleCards} >New Game</button>
       <div className='card-grid'>
         {cards.map((card) => 
-          < SingleCard card={card} key={card.id}/>
+          < SingleCard 
+            card={card} 
+            key={card.id}
+            handleChoice={handleChoice}
+          />
         )}
       </div>
     </div>
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import './SingleCard.css'
 
-function SingleCard({card}) {
+function SingleCard({card, handleChoice}) {
+
+  const handleClick = () => {
+    handleChoice(card)
+  }
+
   return (
     <div className='card'>
       <div>
         <img src={card.src} alt="card front" />
-        <img src='/img/cover.png' alt="card back" />
+        <img 
+          src='/img/cover.png'
+          onClick={handleClick}
+          alt="card back" 
+        />
       </div>
     </div>
   )
